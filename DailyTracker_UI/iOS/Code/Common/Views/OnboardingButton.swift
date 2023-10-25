a070dd3c54633ca421e1873e29e510bffd8778db
Yashraj <yashrajjadhav49@gmail.com>
2023-10-25 11:56:00 +0530

    Created an Onboarding Button

---
+++ DailyTracker_UI/iOS/Code/Common/Views/OnboardingButton.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingButton.swift
+//  DailyTracker_UI
+//
+//  Created by Yashraj jadhav on 24/10/23.
+//
+
+import SwiftUI
+
+struct OnboardingButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingButton()
+}
