9ef9aa6f6c428d67bfec05deda7b592269818a5c
oceaniswater <markflyingbear@gmail.com>
2023-10-26 20:12:46 +0100

    create view extension

--- DailyTracker_UI/iOS/Code/Common/Extensions/ViewExtensions.swift
+++ DailyTracker_UI/iOS/Code/Common/Extensions/ViewExtensions.swift
@@ -15,4 +15,18 @@ extension View {
             self
         }
     }
+    
+    func alert(title: String = "", message: String = "", closeButton: CloseAlertButton = CloseAlertButton(systemName: "xmark"), dismissButton: AlertButton = AlertButton(title: "ok"), isPresented: Binding<Bool>) -> some View {
+        let title   = NSLocalizedString(title, comment: "")
+        let message = NSLocalizedString(message, comment: "")
+    
+        return modifier(CustomAlertModifier(title: title, message: message, closeButton: closeButton, dismissButton: dismissButton, isPresented: isPresented))
+    }
+
+    func alert(title: String = "", message: String = "", closeButton: CloseAlertButton = CloseAlertButton(systemName: "xmark"), primaryButton: AlertButton, secondaryButton: AlertButton, isPresented: Binding<Bool>) -> some View {
+        let title   = NSLocalizedString(title, comment: "")
+        let message = NSLocalizedString(message, comment: "")
+    
+        return modifier(CustomAlertModifier(title: title, message: message, closeButton: closeButton, primaryButton: primaryButton, secondaryButton: secondaryButton, isPresented: isPresented))
+    }
 }
